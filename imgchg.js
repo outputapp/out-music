8f33ed2c09fdcc7dab78367227135159db5fd50f
kylehao <kylehao@gmail.com>
2020-04-01 16:13:12 +0800

    Update imgchg.js

--- imgchg.js
+++ imgchg.js
@@ -1,4 +1,4 @@
-﻿<!--
+<!--
 //
 // ■ 進行動態按鈕圖檔的切換動作
 // 
@@ -26,81 +26,81 @@ img = new Object();
 
 // 「靜音模式」按鈕的圖檔 (已關閉／已開啟)
 img.vmute_off = new Image();
-img.vmute_off.src = "img/btn_mute_off.gif";
+img.vmute_off.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_mute_off.gif";
 img.vmute_on = new Image();
-img.vmute_on.src = "img/btn_mute_on.gif";
+img.vmute_on.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_mute_on.gif";
 
 // 「播放順序模式」按鈕的圖檔 (循序／隨機)
 img.pmode_off = new Image();
-img.pmode_off.src = "img/btn_rndmode_off.gif";
+img.pmode_off.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_rndmode_off.gif";
 img.pmode_on = new Image();
-img.pmode_on.src = "img/btn_rndmode_on.gif";
+img.pmode_on.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_rndmode_on.gif";
 
 // 「是否重複播放」按鈕的圖檔 (不重複／重複)
 img.rept_off = new Image();
-img.rept_off.src = "img/btn_rept_off.gif";
+img.rept_off.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_rept_off.gif";
 img.rept_on = new Image();
-img.rept_on.src = "img/btn_rept_on.gif";
+img.rept_on.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_rept_on.gif";
 
 // 「播放」按鈕的圖檔 (非播放中／播放中／滑鼠移至時)
 img.playt_off = new Image();
-img.playt_off.src = "img/btn_play.gif";
+img.playt_off.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_play.gif";
 img.playt_on = new Image();
-img.playt_on.src = "img/btn_play_on.gif";
+img.playt_on.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_play_on.gif";
 img.playt_ovr = new Image();
-img.playt_ovr.src = "img/btn_play_ovr.gif";
+img.playt_ovr.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_play_ovr.gif";
 
 // 「暫停」按鈕的圖檔 (非暫停中／暫停中／滑鼠移至時)
 img.pauzt_off = new Image();
-img.pauzt_off.src = "img/btn_pauz_off.gif";
+img.pauzt_off.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_pauz_off.gif";
 img.pauzt_on = new Image();
-img.pauzt_on.src = "img/btn_pauz_on.gif";
+img.pauzt_on.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_pauz_on.gif";
 img.pauzt_ovr = new Image();
-img.pauzt_ovr.src = "img/btn_pauz_ovr.gif";
+img.pauzt_ovr.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_pauz_ovr.gif";
 
 // 「停止」按鈕的圖檔 (未被停止／已停止／滑鼠移至時)
 img.stopt_off = new Image();
-img.stopt_off.src = "img/btn_stop.gif";
+img.stopt_off.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_stop.gif";
 img.stopt_on = new Image();
-img.stopt_on.src = "img/btn_stop_on.gif";
+img.stopt_on.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_stop_on.gif";
 img.stopt_ovr = new Image();
-img.stopt_ovr.src = "img/btn_stop_ovr.gif";
+img.stopt_ovr.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_stop_ovr.gif";
 
 // 「上一首曲目」按鈕的圖檔 (一般顯示／滑鼠移至時)
 img.prevt_out = new Image();
-img.prevt_out.src = "img/btn_prev.gif";
+img.prevt_out.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_prev.gif";
 img.prevt_ovr = new Image();
-img.prevt_ovr.src = "img/btn_prev_ovr.gif";
+img.prevt_ovr.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_prev_ovr.gif";
 
 // 「下一首曲目」按鈕的圖檔 (一般顯示／滑鼠移至時)
 img.nextt_out = new Image();
-img.nextt_out.src = "img/btn_next.gif";
+img.nextt_out.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_next.gif";
 img.nextt_ovr = new Image();
-img.nextt_ovr.src = "img/btn_next_ovr.gif";
+img.nextt_ovr.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_next_ovr.gif";
 
 // 「增加音量」按鈕的圖檔 (一般顯示／滑鼠移至時)
 img.vup_out = new Image();
-img.vup_out.src = "img/btn_vup.gif";
+img.vup_out.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_vup.gif";
 img.vup_ovr = new Image();
-img.vup_ovr.src = "img/btn_vup_ovr.gif";
+img.vup_ovr.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_vup_ovr.gif";
 
 // 「減少音量」按鈕的圖檔 (一般顯示／滑鼠移至時)
 img.vdn_out = new Image();
-img.vdn_out.src = "img/btn_vdn.gif";
+img.vdn_out.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_vdn.gif";
 img.vdn_ovr = new Image();
-img.vdn_ovr.src = "img/btn_vdn_ovr.gif";
+img.vdn_ovr.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_vdn_ovr.gif";
 
 // 「顯示播放清單內容」按鈕的圖檔 (一般顯示／滑鼠移至時)
 img.plist_out = new Image();
-img.plist_out.src = "img/btn_plist.gif";
+img.plist_out.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_plist.gif";
 img.plist_ovr = new Image();
-img.plist_ovr.src = "img/btn_plist_ovr.gif";
+img.plist_ovr.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/btn_plist_ovr.gif";
 
 // 顯示播放狀態的 Scope 動態圖檔 (靜止／轉動)
 img.scope_off = new Image();
-img.scope_off.src = "img/scope_off.gif";
+img.scope_off.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/scope_off.gif";
 img.scope_on = new Image();
-img.scope_on.src = "img/scope_on.gif";
+img.scope_on.src = "https://cdn.jsdelivr.net/gh/kylehao/free263/img/scope_on.gif";
 
 }
 
